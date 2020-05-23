2fb7bfb782f2eeef35639ce03b481c54cff9d60d
shgarcia <shgarcia@tgvamericas.net>
2020-05-22 23:14:13 -0400

    agregando population size

--- src/main/java/com/mycompany/nbagenetic/App.java
+++ src/main/java/com/mycompany/nbagenetic/App.java
@@ -51,35 +51,12 @@ public class App {
 
     // 2.) Definition of the fitness function.
     private int eval(Genotype<BitGene> gt) {
-    	//Log.loguear("Evaluando funcion de aptitud ciclo " + cicloCorridaAptitud);
     	
     	List<Player> players = Conversion.chromosomeToPlayers(playersMap,gt);
 
-    	//System.out.println("Longitud lista players: " + players.size());
-        
         if(!this.esValido(players)){
             return  (-1 * (cfg.getMejorPosibleResultadoTotal()));
         }
-    
-    	//PENALIZAR POR REPETICION DE EQUIPO -> Por que penalizamos el mismo equipo? si es correcto y le damos un fitness negativo no lo va a descartar?
-    	//Player rp1 = playersMap.get(players.get(0)); 
-    	//Player rp2 = playersMap.get(players.get(1));
-    	//Player rp3 = playersMap.get(players.get(2));
-    	//Player rp4 = playersMap.get(players.get(3));
-    	//Player rp5 = playersMap.get(players.get(4));
-    	
-    	//Boolean mismoEquipo = rp1.getTeam().equals(rp2.getTeam())
-    	//&& rp2.getTeam().equals(rp3.getTeam())
-    	//&& rp3.getTeam().equals(rp4.getTeam())
-    	//&& rp4.getTeam().equals(rp5.getTeam());
-    	
-    	//if(!mismoEquipo){
-    	//	Log.loguear("Aptitud: descartando por combinacion invalida ciclo " + cicloCorridaAptitud);
-    	//	cicloCorridaAptitud++;
-    	//	//tirarlo abajo totalmente
-        //  	//Seria el opuesto del mejor resultado
-    	//	return  (-1 * (cfg.getMejorPosibleResultadoTotal()));
-    	//}
     	
     	Integer fitnessValue = 0;
         
@@ -97,108 +74,8 @@ public class App {
         cicloCorridaAptitud++;
         return fitnessValue;
     }
-
-    public static void main(String[] args) {
-
-    	System.out.println("COMIENZA EL PROCESO: " + new SimpleDateFormat("hh:mm:ss dd/MM/YYYY").format(new Date()));
-    	
-    	cfg = new Configuracion();
-    	try {
-    		cfg.loadConfig();
-    	}catch(Exception ex) {
-    		ex.printStackTrace();
-    		return;
-    	}
-    	
-    	PlayersRepository playersRepository = PlayersRepository.getInstance();
-        playersMap = playersRepository.getAllPlayersMap();
-
-        System.out.println("PlayersRepository.MAX_PLAYER_ID = '"  + PlayersRepository.MAX_PLAYER_ID + "'");
-        // 1.) Define the genotype (factory) suitable
-        //     for the problem.
-        CHROMOSOME_SIZE = Conversion.getBitCountFor(PlayersRepository.MAX_PLAYER_ID);
-                
-        Factory<Genotype<BitGene>> gtf
-                = Genotype.of(BitChromosome.of(CHROMOSOME_SIZE*cfg.getTamanioEquipo(), 0.5));
-
-        App app = new App();
-        
-        // 3.) Create the execution environment.
-        Engine<BitGene, Integer> engine = Engine
-                .builder(app::eval, gtf)
-                //Experimental 1
-                //.populationSize(100000)
-                //Experimental 2
-                .maximizing()
-                //.offspringFraction(0.7)
-                .constraint(Constraint.of(
-                        phenotype -> {
-                            List<Player> players = Conversion.chromosomeToPlayers(playersMap,phenotype.genotype());
-                            return app.esValido(players);
-                        }
-                ))
-                .survivorsSelector(new RouletteWheelSelector <>())
-                .offspringSelector(new TournamentSelector <>())
-                //.alterers(new Mutator <>(0.55) ,new )
-                .maximalPhenotypeAge(10)
-                //probar este
-                .optimize(Optimize.MAXIMUM)
-                .build();
-        
-        final EvolutionStatistics <Integer , ?>
-        statistics = EvolutionStatistics.ofNumber() ;
-
-        
-        Log.loguear("Comienza corrida del engine");
-        
-        // 4.) Start the execution (evolution) and
-        //     collect the result.
-        Genotype<BitGene> result = engine.stream()
-                .limit(Limits.byFitnessThreshold(690))
-                //.limit(5000)
-                //.limit(cfg.getMaximoCorridas())
-                // Update  the  evaluation  statistics  after each  generation
-                .peek(statistics)
-                .collect(EvolutionResult.toBestGenotype());
-
-        
-        Log.loguear("Obteniendo resultados finales");
-        
-        List<Player> players = Conversion.chromosomeToPlayers(playersMap,result);
-
-        Log.loguear("Cantidad de players de resultados finales: " + (players != null ? players.size():"0"));
-        
-        
-        for(Player p:players) {
-        	if(p != null && p.getId() != null) {
-        		Player realPlayer = playersMap.get(Conversion.enteroBase10AStringBase2(p.getId()));
-        		var line = p.getName() 
-        				+ " - Puntos: " 
-        				+ p.getOverallPoints().toString()
-        				+ " - Altura: "
-                                        + p.getHeight().toString()
-        				+ " - "
-        				+ p.getTeam()
-                                        + " - Posiciones: "
-        				+ p.getPrimaryPosition() + "/" + p.getSecondaryPosition();
-                System.out.println(line + "\n");
-        	}
-        }
-        
-//        for (int i = 0; i < players.size(); i++) {
-//            Player p = playersMap.get(players.get(i));
-//        	if (p != null) {
-//                var line = p.getName() + " - " + p.getOverallPoints().toString();
-//                System.out.println(line + "\n");
-//            }
-//        }
-
-        
-        System.out.println("TERMINO EL PROCESO: " + new SimpleDateFormat("hh:mm:ss dd/MM/YYYY").format(new Date()));
-        
-    }
     
-    
+        
     private boolean esValido(List<Player> players){
         //Verificar que sean 5 jugadores validos
         for(Player p:players){
@@ -246,19 +123,108 @@ public class App {
         
         return true;
     }
-    
-    private static boolean stopCriteria() {
+
+    public static void main(String[] args) {
+
+    	System.out.println("COMIENZA EL PROCESO: " + new SimpleDateFormat("hh:mm:ss dd/MM/YYYY").format(new Date()));
     	
+    	cfg = new Configuracion();
+    	try {
+    		cfg.loadConfig();
+    	}catch(Exception ex) {
+    		ex.printStackTrace();
+    		return;
+    	}
     	
-    	
-    	
-    	
-    	return true;
+    	PlayersRepository playersRepository = PlayersRepository.getInstance();
+        playersMap = playersRepository.getAllPlayersMap();
+
+        System.out.println("PlayersRepository.MAX_PLAYER_ID = '"  + PlayersRepository.MAX_PLAYER_ID + "'");
+        // 1.) Define the genotype (factory) suitable
+        //     for the problem.
+        CHROMOSOME_SIZE = Conversion.getBitCountFor(PlayersRepository.MAX_PLAYER_ID);
+                
+        Factory<Genotype<BitGene>> gtf
+                = Genotype.of(BitChromosome.of(CHROMOSOME_SIZE*cfg.getTamanioEquipo(), 0.5));
+
+        App app = new App();
+        
+        // 3.) Create the execution environment.
+        Engine<BitGene, Integer> engine = Engine
+                .builder(app::eval, gtf)
+                //Experimental 1
+                .populationSize(1000)
+                //Experimental 2
+                .maximizing()
+                //.offspringFraction(0.7)
+                .constraint(Constraint.of(
+                        phenotype -> {
+                            List<Player> players = Conversion.chromosomeToPlayers(playersMap,phenotype.genotype());
+                            return app.esValido(players);
+                        }
+                ))
+                .survivorsSelector(new RouletteWheelSelector <>())
+                .offspringSelector(new TournamentSelector <>())
+                //.alterers(new Mutator <>(0.55) ,new )
+                .maximalPhenotypeAge(10)
+                //probar este
+                .optimize(Optimize.MAXIMUM)
+                .build();
+        
+        final EvolutionStatistics <Integer , ?>
+        statistics = EvolutionStatistics.ofNumber() ;
+
+        
+        Log.loguear("Comienza corrida del engine");
+        
+        // 4.) Start the execution (evolution) and
+        //     collect the result.
+        Genotype<BitGene> result = engine.stream()
+                .limit(Limits.byFitnessThreshold(692))
+                //.limit(5000)
+                //.limit(cfg.getMaximoCorridas())
+                // Update  the  evaluation  statistics  after each  generation
+                .peek(statistics)
+                .collect(EvolutionResult.toBestGenotype());
+
+        
+        Log.loguear("Obteniendo resultados finales");
+        
+        List<Player> players = Conversion.chromosomeToPlayers(playersMap,result);
+
+        Log.loguear("Cantidad de players de resultados finales: " + (players != null ? players.size():"0"));
+        
+        
+        for(Player p:players) {
+        	if(p != null && p.getId() != null) {
+        		Player realPlayer = playersMap.get(Conversion.enteroBase10AStringBase2(p.getId()));
+        		var line = p.getName() 
+        				+ " - Puntos: " 
+        				+ p.getOverallPoints().toString()
+        				+ " - Altura: "
+                                        + p.getHeight().toString()
+        				+ " - "
+        				+ p.getTeam()
+                                        + " - Posiciones: "
+        				+ p.getPrimaryPosition() + "/" + p.getSecondaryPosition();
+                System.out.println(line + "\n");
+        	}
+        }
+        
+//        for (int i = 0; i < players.size(); i++) {
+//            Player p = playersMap.get(players.get(i));
+//        	if (p != null) {
+//                var line = p.getName() + " - " + p.getOverallPoints().toString();
+//                System.out.println(line + "\n");
+//            }
+//        }
+
+        
+        System.out.println("TERMINO EL PROCESO: " + new SimpleDateFormat("hh:mm:ss dd/MM/YYYY").format(new Date()));
+        
     }
     
-    
-    private void runEvolution() {
-    	
-    }
+
+
 
 }
